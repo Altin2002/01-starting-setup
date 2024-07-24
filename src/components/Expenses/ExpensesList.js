93bdd56fbfe9f6401b867ef27a395495fbccd459
Altin Xhema <altinxhema@hotmail.com>
2024-07-24 17:34:22 +0200

    added chart

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -4,7 +4,7 @@ import ExpenseItem from './ExpenseItem';
 
 const ExpensesList = (props) => {
 
-    let expensesContent = <p>No Expenses found.</p>;
+    // let expensesContent = <p>No Expenses found.</p>;
 
     if (props.items.length === 0) {
         return <h2 className='expenses-list__fallback'>Found no expenses.</h2>
