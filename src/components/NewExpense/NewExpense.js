f878ff29552da7e9c0880a9cefb7c26ce1cb0bba
Altin Xhema <altinxhema@hotmail.com>
2024-07-24 17:02:42 +0200

    filtering data

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -2,12 +2,21 @@ import React from 'react'
 import './NewExpense.css'
 import ExpenseForm from './ExpenseForm'
 
-const NewExpense = () => {
-  return (
-    <div className='new-expense'>
-        <ExpenseForm />
-    </div>
-  )
+const NewExpense = (props) => {
+    const saveExpeseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        };
+        console.log(expenseData);
+        props.onAddExpense(expenseData);
+    }
+
+    return (
+        <div className='new-expense'>
+            <ExpenseForm onSaveExpenseData={saveExpeseDataHandler} />
+        </div>
+    )
 }
 
 export default NewExpense
